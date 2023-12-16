02c007f79a20798db0f901e25b827ee2eac1f648
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-12-15 19:15:24 -0600

    Add board print and generate secret code

--- mastermind.rb
+++ mastermind.rb
@@ -5,15 +5,38 @@ require_relative "player.rb"
 
 # Game "Mastermind" class
 class MastermindGame
+
     # Initialize game "Mastermind"
     def initialize()
-        @board = Board.new(12,8)
+    	@secret_code_length = 4
+    	
+        @board = Board.new(12,@secret_code_length*2)
         @name = "Mastermind"
+        @secret_code_colors = ['W','R','B','Y','G','P']
     end
     
     # Principal flow of the game
-    def game 
+    def game
+        @secret_code = generate_random_secret_code()
+        show_secret_code()
+    
         # Print the board at the beginning of the game
 	@board.show()
+    end
+    
+    # Generate the random secret code from the pc
+    def generate_random_secret_code()
+        secret_code = []
+        for i in 1..@secret_code_length do
+            index = rand(@secret_code_length)
+            secret_code.append(@secret_code_colors[index])
+        end
+        return secret_code
+    end
+    
+    # Show the secret code in user-friendly way
+    def show_secret_code()
+        print @secret_code
+        puts ""
     end
 end
